7eb361f34ef67a0b39071f59f4c754e65a523b3b
Louis Chalvignac <louis.chalvignac@gmail.com>
2026-05-06 17:19:38 +0000

    Fin Séance 2

--- README.md
+++ README.md
@@ -20,3 +20,31 @@ Phase 2
 Q4. Oui le salon redemare après le kill. En effet, si on refait un nc localhost 4040 juste après le kill, on a comme proposition le salon général, donc il a redémaré automatiquement.
 
 Q5. Sur un one_for_one, si le process est tué, alors il redémarre automatiquement, mais pour un one_for_all, si un process est tué, alors tout les autres process sont aussi tué et redémarre tous automatiquement (y compris celui qui a été tué a la base).
+
+
+TP2 Programmation Fontcionelle
+
+Fonction Start 
+  def start(host, port) do
+      # TODO : Connecter la socket avec :gen_tcp.connect/3
+      # TODO : Options : [:binary, packet: :line, active: false]
+      case :gen_tcp.connect(to_charlist(host), port, [:binary, packet: :line, active: false]) do
+        {:ok, socket} ->
+          # TODO : Appeler la fonction rencontre(socket) pour le pseudo et le salon
+          rencontre(socket)
+          # TODO : Lancer le receiver dans un Task.async : fn -> receive_loop(socket) end
+          receive_task = Task.async(fn -> receive_loop(socket) end)
+          # TODO : Lancer le sender dans un Task.async : fn -> send_loop(socket) end
+          send_task = Task.async(fn -> send_loop(socket) end)
+          # TODO : Attendre les deux tasks avec Task.await/2 (timeout: :infinity)
+          Task.await(receive_task, :infinity)
+          Task.await(send_task, :infinity)
+        {:error, reason} ->
+          IO.puts("Erreur de connexion : #{reason}")
+      end
+  end
+
+2.3 Robustesse OTP
+Elle permetrait de savoir si par exemple un client se connecte et se déconnecte en boucle (pour une attaque de déni de service par exemple), ou si un utilisateur n'arrive pas à se connecter.
+En somme un suivi des processus nous aide a résoudre plusieurs problème que nous ne pourrions voir avec un redemarage automatique.
+
--- client/lib/client.ex
+++ client/lib/client.ex
@@ -5,45 +5,51 @@ defmodule MiniDiscord.Client do
   host : nom type 'xxxbore.pub'
   port : entier ex: 4040
   """
-  def start(host, port) do
-      # TODO : Connecter la socket avec :gen_tcp.connect/3
-      # TODO : Options : [:binary, packet: :line, active: false]
-      case :gen_tcp.connect(to_charlist(host), port, [:binary, packet: :line, active: false]) do
-        {:ok, socket} ->
-          # TODO : Appeler la fonction rencontre(socket) pour le pseudo et le salon
-          rencontre(socket)
-          # TODO : Lancer le receiver dans un Task.async : fn -> receive_loop(socket) end
-          receive_task = Task.async(fn -> receive_loop(socket) end)
-          # TODO : Lancer le sender dans un Task.async : fn -> send_loop(socket) end
-          send_task = Task.async(fn -> send_loop(socket) end)
-          # TODO : Attendre les deux tasks avec Task.await/2 (timeout: :infinity)
-          Task.await(receive_task, :infinity)
-          Task.await(send_task, :infinity)
-        {:error, reason} ->
-          IO.puts("Erreur de connexion : #{reason}")
-      end
+def start(host, port) do
+  connect_with_retry(host, port, 1)
+end
+
+defp connect_with_retry(host, port, attempt) do
+  # TODO : Tenter :gen_tcp.connect avec les bonnes options
+  # TODO : Si {:ok, socket} -> handshake(socket) puis lancer les deux loops
+   # TODO : Si {:error, reason} ->
+  # TODO :   Afficher "Tentative #{attempt} échouée : #{reason}"
+  # TODO :   Attendre 2 secondes avec :timer.sleep(2000)
+  # TODO :   Rappeler connect_with_retry(host, port, attempt + 1)
+  case :gen_tcp.connect(to_charlist(host), port, [:binary, active: false]) do
+    {:ok, socket} ->
+      rencontre(socket)
+      receive_task = Task.async(fn -> receive_loop(socket) end)
+      send_task = Task.async(fn -> send_loop(socket) end)
+      Task.await(receive_task, :infinity)
+      Task.await(send_task, :infinity)
+    {:error, reason} ->
+      IO.puts("Tentative #{attempt} échouée : #{reason}")
+      :timer.sleep(2000)
+      connect_with_retry(host, port, attempt + 1)
   end
+end
 
   defp rencontre(socket) do
       # TODO : Lire les messages du serveur avec recv_print(socket)
       recv_print(socket)
       # TODO : Envoyer le pseudo choisi par l'utilisateur avec IO.gets/1
-      pseudo = IO.gets("Entrez le pseudo : ")
-      :gen_tcp.send(socket, String.to_charlist(pseudo))
+      pseudo = IO.gets("")
+      :gen_tcp.send(socket, to_charlist(pseudo))
       # TODO : Lire la suite (liste des salons)
       recv_print(socket)
-      salon = IO.gets("Entrez le nom du salon : ")
-      :gen_tcp.send(socket, String.to_charlist(salon))
+      salon = IO.gets("")
+      :gen_tcp.send(socket, to_charlist(salon))
       # TODO : Envoyer le nom du salon
       # TODO : Lire la confirmation
       recv_print(socket)
-
   end
 
   defp recv_print(socket) do
       case :gen_tcp.recv(socket, 0) do
-        {:ok, msg} -> IO.write(msg)
-        {:error, _} -> IO.puts("Déconnecté")
+        {:ok, msg} ->
+          IO.write(msg)
+        {:error, _} -> :ok
       end
   end
 
@@ -51,18 +57,20 @@ defmodule MiniDiscord.Client do
       # TODO : Appeler :gen_tcp.recv(socket, 0) — bloquant jusqu'à réception
       case :gen_tcp.recv(socket, 0) do
       # TODO : Si {:ok, msg} -> afficher avec IO.write/1 et rappeler receive_loop
-        {:ok, msg} -> IO.write(msg)
+        {:ok, msg} ->
+          IO.write(msg)
+          receive_loop(socket)
       # TODO : Si {:error, _} -> afficher "Déconnecté" et arrêter
-        {:error, _} -> IO.puts("Déconnecté")
+        {:error, reason} ->
+          IO.puts("\nDéconnecté (#{reason})")
       end
-      receive_loop(socket)
   end
 
   defp send_loop(socket) do
       # TODO : Lire depuis le clavier avec IO.gets("")
     message = IO.gets("")
       # TODO : Envoyer au serveur avec :gen_tcp.send/2
-    :gen_tcp.send(socket, String.to_charlist(message))
+    :gen_tcp.send(socket, to_charlist(message))
       # TODO : Rappeler send_loop(socket)
     send_loop(socket)
   end
